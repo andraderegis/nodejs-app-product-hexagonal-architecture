23349ed9fef3ace8cb5e981373fb002d28883f64
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2021-09-20 16:17:58 -0300

    feat: added OK, NOT_FOUND and SERVER_ERROR response helpers

--- src/adapters/inbound/helpers/http-response-helpers.ts
+++ src/adapters/inbound/helpers/http-response-helpers.ts
@@ -2,3 +2,20 @@ export type HttpResponse<T = unknown> = {
   statusCode: number;
   data: T;
 };
+
+class HttpResponseHelper {
+  static makeReponse(statusCode: number, data: unknown): HttpResponse {
+    return {
+      statusCode,
+      data
+    };
+  }
+}
+
+const OK = (data: unknown): HttpResponse => HttpResponseHelper.makeReponse(200, data);
+
+const NOT_FOUND = (): HttpResponse => HttpResponseHelper.makeReponse(404, undefined);
+
+const SERVER_ERROR = (error: unknown): HttpResponse => HttpResponseHelper.makeReponse(500, error);
+
+export { OK, NOT_FOUND, SERVER_ERROR };
