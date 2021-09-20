74b3fc27b49187834f38a086f87fcbf4d9b22335
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2021-09-20 16:23:14 -0300

    refactor(adapters:controllers): added http response helpers to resolve http responses

--- src/adapters/inbound/controllers/get-products-controller.ts
+++ src/adapters/inbound/controllers/get-products-controller.ts
@@ -1,10 +1,17 @@
 import ProductServicePort from '@application/ports/product-service-port';
+
 import {
   ControllerInterface,
   GetProductsHttpRequestParams,
   HttpRequest
 } from '@adapters/inbound/interfaces';
-import { HttpResponse } from '../helpers/http-response-helpers';
+
+import {
+  HttpResponse,
+  OK,
+  NOT_FOUND,
+  SERVER_ERROR
+} from '@adapters/inbound/helpers/http-response-helpers';
 
 export class GetProductController implements ControllerInterface {
   constructor(private readonly productService: ProductServicePort) {}
@@ -14,15 +21,10 @@ export class GetProductController implements ControllerInterface {
   ): Promise<HttpResponse<unknown>> {
     try {
       const products = await this.productService.get(request.data.id);
-      return {
-        statusCode: products ? 200 : 404,
-        data: products
-      };
+
+      return products ? OK(products) : NOT_FOUND();
     } catch (error) {
-      return {
-        statusCode: 500,
-        data: error
-      };
+      return SERVER_ERROR(error);
     }
   }
 }
