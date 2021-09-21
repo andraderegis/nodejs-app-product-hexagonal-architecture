d74852c2c56e143af483985ef45f4f1c9498e2e4
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2021-09-21 16:41:54 -0300

    feat: added create and bad request response helpers

--- src/adapters/inbound/helpers/http-response-helpers.ts
+++ src/adapters/inbound/helpers/http-response-helpers.ts
@@ -1,21 +1,31 @@
+import { ServerError } from '../errors/http';
+
 export type HttpResponse<T = unknown> = {
   statusCode: number;
   data: T;
+  error?: Error;
 };
 
 class HttpResponseHelper {
-  static makeReponse(statusCode: number, data: unknown): HttpResponse {
+  static makeReponse(statusCode: number, data: unknown, error?: Error): HttpResponse {
     return {
       statusCode,
-      data
+      data,
+      error
     };
   }
 }
 
 const OK = (data: unknown): HttpResponse => HttpResponseHelper.makeReponse(200, data);
 
+const CREATED = (data: unknown): HttpResponse => HttpResponseHelper.makeReponse(201, data);
+
+const BAD_REQUEST = (error: Error): HttpResponse =>
+  HttpResponseHelper.makeReponse(400, undefined, error);
+
 const NOT_FOUND = (): HttpResponse => HttpResponseHelper.makeReponse(404, undefined);
 
-const SERVER_ERROR = (error: unknown): HttpResponse => HttpResponseHelper.makeReponse(500, error);
+const SERVER_ERROR = (error: Error): HttpResponse =>
+  HttpResponseHelper.makeReponse(500, undefined, new ServerError(error));
 
-export { OK, NOT_FOUND, SERVER_ERROR };
+export { OK, CREATED, BAD_REQUEST, NOT_FOUND, SERVER_ERROR };
