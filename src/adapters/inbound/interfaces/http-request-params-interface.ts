839756f353debf47785ae7b59a78ab6b2e231e65
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2021-09-21 16:40:36 -0300

    feat: added CreateProductHttpRequestParams

--- src/adapters/inbound/interfaces/http-request-params-interface.ts
+++ src/adapters/inbound/interfaces/http-request-params-interface.ts
@@ -3,3 +3,8 @@ export interface HttpRequestParams {}
 export interface GetProductsHttpRequestParams extends HttpRequestParams {
   id: string;
 }
+
+export interface CreateProductHttpRequestParams extends HttpRequestParams {
+  name: string;
+  price: number;
+}
