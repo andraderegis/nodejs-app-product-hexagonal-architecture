165a2921d945638fca77a01b50797527d2d58d1d
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2021-10-04 12:00:57 -0300

    test(application:domain): added product constructor tests

--- src/application/domain/tests/product.spec.ts
+++ src/application/domain/tests/product.spec.ts
@@ -1,6 +1,38 @@
 import Product, { ProductStatus } from '@application/domain/product';
+import { mocked } from 'ts-jest/utils';
+import { v4, validate } from 'uuid';
+
+jest.mock('uuid', () => ({
+  validate: jest.fn(),
+  v4: jest.fn()
+}));
 
 describe('Product Tests', () => {
+  let validateSpy: jest.Mock;
+  let v4Spy: jest.Mock;
+
+  beforeAll(() => {
+    validateSpy = jest.fn().mockReturnValue(true);
+    mocked(validate).mockImplementation(validateSpy);
+
+    v4Spy = jest.fn().mockReturnValue('49fe94e7-e052-4925-bb6c-15274c18c602');
+    mocked(v4).mockImplementation(v4Spy);
+  });
+  describe('constructor method', () => {
+    it('should be create with informed id', () => {
+      const id = v4Spy();
+      const product = new Product('notebook', 5000, id);
+
+      expect(validateSpy).toBeCalled();
+      expect(id).toEqual(product.id);
+    });
+    it('should be create without informed id', () => {
+      const product = new Product('notebook', 5000);
+
+      expect(v4Spy).toBeCalled();
+      expect(product).toBeDefined();
+    });
+  });
   describe('enable method', () => {
     it('should be enable product', async () => {
       let product = new Product('notebook', 5000);
@@ -35,19 +67,4 @@ describe('Product Tests', () => {
       );
     });
   });
-
-  // describe('validate method', () => {
-  //   it('should be valid product', async () => {
-  //     const product = new Product('notebook', 0);
-
-  //     await expect(product.validate()).resolves.toBeUndefined();
-  //   });
-  //   it('should be invalid product. price is minor than 0', async () => {
-  //     const product = new Product('notebook', -10);
-
-  //     await expect(product.validate()).rejects.toThrowError(
-  //       'The price must be greater or equal zero.'
-  //     );
-  //   });
-  // });
 });
