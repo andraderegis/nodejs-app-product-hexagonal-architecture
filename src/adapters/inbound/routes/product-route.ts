5f0e40b6485756fb8c5038178541637df0f4927e
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2021-10-05 15:35:45 -0300

    feat(adapters:inbound): added update product route

--- src/adapters/inbound/routes/product-route.ts
+++ src/adapters/inbound/routes/product-route.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import { CreateProductControllerFactory } from '@adapters/inbound/factories';
+import {
+  CreateProductControllerFactory,
+  UpdateProductControllerFactory,
+  GetProductControllerFactory
+} from '@adapters/inbound/factories';
 import { requestHandler } from '@adapters/inbound/helpers/http-express-helpers';
-import { GetProductControllerFactory } from '../factories/get-product-controller-factory';
 
 export default (router: Router): void => {
   router.post('/v1/products', requestHandler(new CreateProductControllerFactory().create()));
+  router.patch('/v1/products', requestHandler(new UpdateProductControllerFactory().create()));
   router.get('/v1/products/:id', requestHandler(new GetProductControllerFactory().create()));
 };
